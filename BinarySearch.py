0627d516d02875d7c44dae482783b806995ab6be
Joshua Cancio <jd.cancio1@gmail.com>
2026-01-20 14:08:30 -0500

    finished binary search function

--- BinarySearch.py
+++ BinarySearch.py
@@ -1,9 +1,20 @@
 nums = [1, 3, 5, 2, 9]
 
-def BinarySearch(self, inputArray, value):
+def BinarySearch(self, inputArray, target):
     inputArray = sorted(inputArray)
     l, r = nums[0], nums[len(nums)]
-    while l < r:
     
+    while l < r:
+        current = (r - l) // 2
+        if current < target:
+            r = current
+        elif current > target:
+            l = current
+        elif current == target:
+            return current
+        else:
+            return -1
+    return
+
 
         
