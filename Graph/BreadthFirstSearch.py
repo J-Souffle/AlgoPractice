1ef8c608f02ce1f9f410418922ac6ff656434178
Joshua Cancio <jd.cancio1@gmail.com>
2026-01-20 15:01:45 -0500

    Created nodes

--- Graph/BreadthFirstSearch.py
+++ Graph/BreadthFirstSearch.py
@@ -3,4 +3,12 @@ class TreeNode:
         self.node = value
         self.left = None
         self.right = None
-    
+    def bfs(self, node):
+
+
+val1 = TreeNode(1)
+val2 = TreeNode(2)
+val3 = TreeNode(3)
+val4 = TreeNode(4)
+val5 = TreeNode(5)
+        
