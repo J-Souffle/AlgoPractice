64dc30361d7eb39868cba0e2793ca2ce51c0fbe8
Joshua Cancio <jd.cancio1@gmail.com>
2026-01-22 14:15:08 -0500

    implemented bfs function

--- Graph/BreadthFirstSearch.py
+++ Graph/BreadthFirstSearch.py
@@ -1,11 +1,37 @@
+from collections import deque 
+# always import this for bfs ^^^
+
 class TreeNode:
     def __init__(self, value):
         self.node = value
         self.left = None
         self.right = None
     
-    # Base case : rightmost and last level node, Recursive case: elements still in stack
-    def bfs(self, node):
+    # use a queue
+    def bfs(self):
+        # 1. Initialize the queue with the root (self)
+        queue = deque([self])
+        result = []
+
+        # loop until queue is empty
+        while queue:
+            # pop the oldest node (left side of dequeue)
+            current = queue.popleft
+            result.append(current.val)
+
+            # add children to the back of the queue
+            if current.left:
+                queue.append(current.left)
+            if current.right:
+                queue.append(current.right)
+            
+        return result
+
+            
+
+    
+    # def printTree(self, node):
+        
         
 
 
@@ -23,4 +49,5 @@ val2.left = val4
 val2.right = val5
 val3.right = val6
 val3.left = val7
-        
+
+print(val1.bfs())
