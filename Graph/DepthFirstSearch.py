e2fce9a86d947bf40fba4928f11197095b917b52
Joshua Cancio <jd.cancio1@gmail.com>
2026-01-22 14:44:28 -0500

    Finished dfs recursive

--- Graph/DepthFirstSearch.py
+++ Graph/DepthFirstSearch.py
@@ -5,14 +5,28 @@ class TreeNode:
         self.right = None
     
     # recursion
-    def dfs_recursive(self, node):
-        # base case
-        if not node:
-            return
+def dfs_recursive(node):
+    # base case
+    if not node:
+        return
 
-        # process current node
-        print(node.val)
+    # process current node
+    print(node.val)
 
-        dfs_recursive(node.right)
-        dfs_recursive(node.left)
+    dfs_recursive(node.right)
+    dfs_recursive(node.left)
+
+val1 = TreeNode(1)
+val1.left = TreeNode(2)
+val1.right = TreeNode(3)
+val1.left.left, val1.left.right = TreeNode(4), TreeNode(5)
+val1.right.left, val1.right.right = TreeNode(6), TreeNode(7)
+
+#       1
+#     /   \
+#    2     3
+#  /  \   /  \
+# 4   5  6    7
+ 
+print(dfs_recursive(val1))
 
